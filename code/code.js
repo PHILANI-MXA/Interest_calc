4f29528427c19a53f43fee1d414fc2e47c6f0090
PHILANI-MXA <3911755@myuwc.ac.za>
2022-06-20 14:03:49 +0200

    initial commit

---
+++ code/code.js
@@ -0,0 +1,13 @@
+var principle = document.getElementById("principle");
+var roi = document.getElementById("roi");
+var years = document.getElementById("years");
+var outputspan = document.getElementById("outputspan");
+
+//itvoyagers.in
+function countCI() {
+    p = parseFloat(principle.value);
+    r = parseFloat(roi.value);
+    t = parseFloat(years.value);
+    ci = p * (Math.pow((1 + r / 100), t));
+    outputspan.innerHTML = ci;
+}
